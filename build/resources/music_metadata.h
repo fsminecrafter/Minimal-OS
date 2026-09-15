66fa9dbfc982b4a8bfdb9151518ad40a97969a6e
Joel_minecrafter <121200076+fsminecrafter@users.noreply.github.com>
2026-09-15 11:45:30 +0000

    Fix

--- build/resources/music_metadata.h
+++
@@ -1,7 +0,0 @@
-// Audio metadata for /workspaces/Minimal-OS/build/resources/music.o
-#define AUDIO_FORMAT "IADPCM"
-#define AUDIO_LENGTH 44
-#define AUDIO_DATALEN 2115994
-#define AUDIO_VOLUME 80
-#define AUDIO_SAMPLERATE 48000
-#define AUDIO_CHANNELS 2

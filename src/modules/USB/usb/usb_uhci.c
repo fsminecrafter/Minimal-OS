9c5fbc892d388d0c437ab1c954a2d2d7b06639ab
Joel_minecrafter <121200076+fsminecrafter@users.noreply.github.com>
2026-09-18 11:33:15 +0200

    Update usb_uhci.c

--- src/modules/USB/usb/usb_uhci.c
+++ src/modules/USB/usb/usb_uhci.c
@@ -38,6 +38,11 @@ static uint8_t keyboard_int_buffer[8];
 static uint8_t keyboard_toggle = 0;
 static bool keyboard_is_low_speed = false;
 
+static uhci_td_t* mouse_int_td = NULL;
+static uint8_t mouse_int_buffer[8];
+static uint8_t mouse_toggle = 0;
+static bool mouse_is_low_speed = false;
+
 // ===========================================
 // UHCI REGISTERS AND CONSTANTS
 // ===========================================

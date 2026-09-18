9d47ba9ccbafd861e5538a70e72de42a518dc2b4
Joel_minecrafter <121200076+fsminecrafter@users.noreply.github.com>
2026-09-18 11:32:16 +0200

    Update uhci.h

--- src/intf/usb/uhci.h
+++ src/intf/usb/uhci.h
@@ -16,10 +16,15 @@ bool uhci_interrupt_transfer(uint8_t dev_addr, uint8_t endpoint, void* buffer, u
 bool usb_enumerate_device(uint8_t port, bool low_speed);
 bool usb_parse_configuration(usb_device_t* dev, uint8_t* config_data, uint16_t length);
 bool usb_keyboard_init_device(usb_device_t* dev);
+bool usb_mouse_init_device(usb_device_t* dev);
 
 // Keyboard polling
 void usb_poll_keyboard(usb_device_t* dev);
 void uhci_keyboard_interrupt_init(usb_device_t* dev);
+
+// Mouse polling
+void usb_poll_mouse(usb_device_t* dev);
+void uhci_mouse_interrupt_init(usb_device_t* dev);
 
 // Returns the usb_hw_driver_t vtable for registration with
 // usb_manager_register_driver(). Prefer this + usb_manager_init()
